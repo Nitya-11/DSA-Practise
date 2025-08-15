8422ee779583022ca995b5e8091b9cf84098f393
nitya singh <nityasingh559@gmail.com>
2025-08-15 22:42:01 +0530

    Pairs of array

--- ArrayCC.java
+++ ArrayCC.java
@@ -3,27 +3,23 @@ import java.util.*;
 public class ArrayCC {
 
   // Reverse of array 
-  
-  public static void reverse(int num[]) {
-    int first =0, last = num.length-1;
+  public static void pairsArray(int num[]) {
+    int tp = 0;
+    for(int i=0;i<num.length;i++) {
+      int curr = num[i];
 
-    while(first < last) {
-      int temp = num[last];
-      num[last] = num[first];
-      num[first] = temp;
-
-      first++;
-      last --;
+      for(int j=i+1;j<num.length;j++) {
+        System.out.print("(" + curr + "," + num[j] + ")");
+        tp++;
+      }
+      System.out.println();
     }
+    System.out.println("total pairs: "+tp);
   }
+
 public static void main(String args[]) {
   int num[] = {2,4,6,7,8,10};
-
-  reverse(num);
-  for(int i=0;i<num.length;i++) {
-    System.out.print(" "+num[i]);
-  }
-   System.out.println();
+  pairsArray(num);
 }
 
 }
