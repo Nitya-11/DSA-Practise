2ada67029dbba77f883b58845bf094f517a40588
nitya singh <nityasingh559@gmail.com>
2025-08-15 20:01:54 +0530

    Reverse of the array

--- ArrayCC.java
+++ ArrayCC.java
@@ -1,6 +1,29 @@
 import java.util.*;
 
 public class ArrayCC {
-public 
+
+  // Reverse of array 
+  
+  public static void reverse(int num[]) {
+    int first =0, last = num.length-1;
+
+    while(first < last) {
+      int temp = num[last];
+      num[last] = num[first];
+      num[first] = temp;
+
+      first++;
+      last --;
+    }
+  }
+public static void main(String args[]) {
+  int num[] = {2,4,6,7,8,10};
+
+  reverse(num);
+  for(int i=0;i<num.length;i++) {
+    System.out.print(" "+num[i]);
+  }
+   System.out.println();
+}
 
 }
